5232299dd27a823a3189f12c15af718e7e2739c3
Walid Ouahrani <104162594+walid998112@users.noreply.github.com>
2025-02-18 12:15:39 +0100

    Update Footer.js
    changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Idées recette by walid pour azza. All Rights Reserved.</p>
+      <p>. All Rights Reserved.</p>
       <div className="social-media">
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">📘</a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">🐦</a>
